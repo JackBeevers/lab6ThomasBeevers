67a0fcd8d7f0a4b9110d85e549d2db32b8f2b81b
JackBeevers <113144110+JackBeevers@users.noreply.github.com>
2022-10-13 16:12:31 -0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿
+void StackNoParams();
+{
+    int a = 35;
+    int b = 25;
+    int c = 45;
+    int d = 55;
+    int e = 65; 
+};
+
+void StackWithParams(int a, int b, int c);
+{
+
+};
+
+void StackWithHeap(int a, int b, int c);
+{
+int[]g = new int[]
+int f = 65;
+int g = 75;
+
+}
