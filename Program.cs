4859e862af2105aa06b79eaa8e3a80d3a13a68d3
JackBeevers <113144110+JackBeevers@users.noreply.github.com>
2022-12-09 15:38:27 -0700

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,22 +1,42 @@
-﻿
-void StackNoParams();
-{
-    int a = 35;
-    int b = 25;
-    int c = 45;
-    int d = 55;
-    int e = 65; 
-};
+﻿void StackNoParams () {
+int a ;
+int b ;
+int c ;
+int d ;
+int e ;
+a = 1;
+b = 2;
+c = 3;
+d = 4; 
+e = 5; 
+ }
+StackNoParams();
 
-void StackWithParams(int a, int b, int c);
-{
-
-};
-
-void StackWithHeap(int a, int b, int c);
-{
-int[]g = new int[]
-int f = 65;
-int g = 75;
-
+void StackWithParams (int a2, int b2, int c2){
+a2 = 3;
+b2 = 4; 
+c2 = 5; 
 }
+
+
+void StackWithHeap (int a3, int b3, int c3, string d3){
+a3 = 1;
+b3 = 2; 
+c3 = 3;
+d3 = "hello";
+int e3 ;
+int f3 ;
+e3 = 7;
+f3 = 10;
+string g3; 
+g3 = "my name is blake ";
+}
+
+
+void StackOverFlow (){
+    int a4 = 100;
+    string b4 = "hey man";
+    Console.WriteLine (a4 + b4);
+    StackOverFlow();
+}
+StackOverFlow();
